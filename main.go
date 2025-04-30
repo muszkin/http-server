332dc60c36d2b5c9d25fc62af3736d5726b21b95
muszkin <muszkin@gmail.com>
2025-04-30 08:25:10 +0200

    update

--- main.go
+++ main.go
@@ -7,12 +7,21 @@ import (
 func main() {
 	const fileRootPath = "."
 	const port = "8080"
-	serveMux := http.NewServeMux()	
-	serveMux.Handle("/", http.FileServer(http.Dir(fileRootPath)))
-
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(fileRootPath))))
+	serveMux.HandleFunc("/healthz", readinessHandler)
 	server := http.Server{
 		Handler: serveMux,
-		Addr: ":" + port,
+		Addr:    ":" + port,
 	}
 	server.ListenAndServe()
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	_, err := w.Write([]byte("OK"))
+	if err != nil {
+		return
+	}
+	r.Header.Set("Content-Type", "text/plain; charset=utf-8")
 }
